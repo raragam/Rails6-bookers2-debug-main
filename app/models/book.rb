aca1eaef98ca53126d932be69106294ee97ed35c
EC2 Default User <ec2-user@ip-172-31-31-32.ap-northeast-1.compute.internal>
2022-06-25 13:26:34 +0000

    Initial Commit

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,8 @@
 class Book < ApplicationRecord
 
   validates :title,presence:true
-  validates :body,presence:true,length:{maximum:200}
+  validates :body,presence:true
+  validates :body, length: { minimum: 1, maximum: 200 }
 
   belongs_to :user
 
